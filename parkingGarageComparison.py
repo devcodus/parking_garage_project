b476d16382d25030bb30179c623d44d1f1fd9ca6
David <David@Biancas-MacBook-Pro.local>
2022-12-27 18:18:00 -0600

    adding leaveGarage

--- parkingGarageComparison.py
+++ parkingGarageComparison.py
@@ -7,57 +7,43 @@ class parkingGarage:
 
     #def __init__(self):
     #this is used for specific information, not for something as general as a parking garage
-    #we're not making instances of 'cars' or 'animals'
+
 
 #how many parking spaces are left:
    
     def available(self):
         if self.garage['spaces'] == 0:
             print(f"Unfortunately, we have {self.garage['spaces']} parking spaces left.")
-            return self.garage['spaces'] ##why are we returning self.garage['spaces'] here?
+            return self.garage['spaces']
         else:
-            print(f"We have {self.garage['spaces']} left")##should we also return self.garage['spaces'] here?
-            #so how much time do you want to stay? lock it in to the key of the dictionary
-            #parking spot 1 : 2 Hrs
+            print(f"We have {self.garage['spaces']} left")
 
 #pay for a spot in the garage when they arrive and links to ticket number and time so that time in lot is billed correctly.
     def takeTicket(self):
         time = input("How many hours do you plan to stay with us today?")
         if time.isdigit():
             #is.digit() checks to see if the value inputted is actually an integer or not
-            currentTicket = self.garage['ticketsTaken'] ##why are we setting currentTicket to self.garage['ticketsTaken']???
-            self.tickets[currentTicket] = time 
+            currentTicket = self.garage['ticketsTaken']
+            self.tickets[currentTicket] = time
             print (f'Your ticket number is: {currentTicket}')
             self.garage['ticketsTaken'] += 1
             self.garage['spaces'] -= 1
         else:
             print('Invalid entry, please try again!')
             #call function again
-            self.takeTicket() ##why are you calling the function again here? do we need a quit option?  
+            self.takeTicket()   
 
 #current ticket is key and the value will be time. whenever you store ticket, it will be storing ticket and time
 
     def leaveGarage(self):
+            payment = int(time) * 2
+            print (f'You owe: ${payment}.') 
             payoption = input('Would you like to pay with cash or card?')
-            if payoption.lower() == 'cash' or 'card': 
-                payment = int(self.time) * 2
-                print (f'You owe: ${payment}. Thank you for visiting, you have 15 minutes to leave the garage') 
-                self.garage['ticketsTaken'] -= 1
-                self.garage['spaces'] += 1
-            else: 
-                print(f'You must pay ${payment} before leaving the lot. Please try again.')
-                self.leaveGarage()
-           
-            
-
-            #if they paid they can leave, if they don't then they gotta stay
-            #assign ticket number and locate tickets in ticket dictionary
-            #opposite of what we had up top ... += 1
-
+    
             
             
 #Ticket number, locate ticket in tickets dictionary, then multiply time associated with ticket by rate then you owe blah blah cash or card.
-#increment spaces up to account for new availability ##we still have to add incrementSpaces code yeah?
+#increment spaces up to account for new availability
            
 
 
